d1e2a707a202237bb771f55588e54b9cd4d03a05
Ray Cancino <cancinoray@gmail.com>
2023-05-22 01:28:11 +0800

    admin basic design

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,17 @@ export default function Home() {
   const {data: session} = useSession()
   if(!session) {
     return (
-      <div className='bg-blue-900 w-screen h-screen flex items-center'>
-      <div className='text-center w-full'>
-      <button className='bg-white p-2 rounded-lg px-4' onClick={() => signIn('google')}>Login with Google</button>
+      <div className='flex items-center w-screen h-screen bg-blue-900'>
+      <div className='w-full text-center'>
+      <button className='p-2 px-4 bg-white rounded-lg' onClick={() => signIn('google')}>Login with Google</button>
       </div>
     </div>
     )
   }
 
   return (
-    <div className='bg-blue-900 min-h-screen'>
-    <Nav />
+    <div className='min-h-screen bg-blue-900'>
+      <Nav />
       <div className='p-2'>logged in {session.user.email} </div>
     </div>
   )
